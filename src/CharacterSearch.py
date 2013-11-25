15fcdb01db271e522b0cfe0cd95bf9f0688a425c
jmuel <jmuel11@gmail.com>
2013-11-24 22:19:39 -0600

    Fleshed out some of the initial stuff for character search

--- src/CharacterSearch.py
+++ src/CharacterSearch.py
@@ -5,5 +5,13 @@ class CharacterSearch:
 
     html = None
 
-    def __init__(self, url):
+    def __init__(self, characterName):
+        url = self.getUrl(characterName)
         html = urllib2.urlopen(url)
+
+    @staticmethod
+    def getUrl(characterName):
+        characterName = characterName.replace(" ", "+")
+        url = "http://eu.finalfantasyxiv.com/lodestone/character/"
+        url += "?q=" + characterName
+        return url
--- test/unit/CharacterSearchTest.py
+++ test/unit/CharacterSearchTest.py
@@ -1,4 +1,5 @@
 import unittest
+from src.CharacterSearch import CharacterSearch
 
 
 class CharacterSearchTest(unittest.TestCase):
@@ -7,3 +8,9 @@ class CharacterSearchTest(unittest.TestCase):
 
     def setUp(self):
         characterSearcher = None
+
+    def testGetUrlAddsPlusSigns(self):
+        url = CharacterSearch.getUrl("Arn Eldgrimm")
+
+        self.assertEqual("http://eu.finalfantasyxiv.com/lodestone/character/?q=Arn+Eldgrimm", url)
+
